8a492e4b97288095496845105eb1b33013a386f9
Conall <conallimlah@gmail.com>
2022-12-07 21:43:28 +0900

    add basic models, serializers and view

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    """ Category represents a group of users """
+    name = models.CharField(max_length=50, blank=False)
+    members = models.ManyToManyField(
+        User, through="GroupMember", through_fields=("group", "user"))
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class GroupMember(models.Model):
+    """ GroupMember is the through table for groups and users """
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    verified = models.BooleanField(default=False)
+    verification_code = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    """ Category represents an Expense category """
+    title = models.CharField(max_length=50, blank=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Expense(models.Model):
+    """ Expense represents a single user expense """
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    amount = models.IntegerField(blank=False)
+    paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} - ¥{self.amount}"
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import views, status
+from rest_framework.exceptions import ParseError, ValidationError
+from rest_framework.parsers import JSONParser
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .serializers import GroupSerializer
+from .models import Group
 
-# Create your views here.
+
+class GroupAPIView(views.APIView):
+    """ APIView for managing groups """
+
+    def post(self, request: Request) -> Response:
+        """ POST request to create a group """
+        try:
+            data = JSONParser().parse(request)
+            req_user = data.pop("members")
+            print(dict(req_user[0]))
+            data["members"] = [dict(req_user[0])]
+            print("RE", data)
+            serializer = GroupSerializer(
+                data=data, partial=True, context=dict(req_user[0]))
+            if not serializer.is_valid(raise_exception=True):
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data)
+        except (ParseError):
+            return Response({"result": "error", "description": "could not parse request"}, status=status.HTTP_400_BAD_REQUEST)
+        except (ValidationError):
+            print("E", serializer.errors)
+            return Response({"result": "error", "description": "invalid request"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GroupDetailsAPIView(views.APIView):
+
+    def get_object(self, pk: int) -> Group:
+        try:
+            return Group.objects.get(pk=pk)
+        except Group.DoesNotExist:
+            raise Http404
+
+    def get(self, _request: Request, pk: int, _format=None) -> Response:
+        """ GET request returns a group """
+        group = self.get_object(pk=pk)
+        serializer = GroupSerializer(group)
+        return Response(serializer.data)
