25c99a9ea8d1cd3816f71c7c7c4a1f84c55fb270
Conall <conallimlah@gmail.com>
2023-01-16 23:16:37 +0900

    migrate

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+from django.db import models
+from django.utils import timezone
 
 
 class Group(models.Model):
@@ -7,6 +9,8 @@ class Group(models.Model):
     name = models.CharField(max_length=50, blank=False)
     members = models.ManyToManyField(
         User, through="GroupMember", through_fields=("group", "user"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -18,6 +22,18 @@ class GroupMember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
     verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Currency(models.Model):
+    """ Currency represents an expenses currency """
+    name = models.CharField(max_length=3, blank=False)
+    decimals = models.PositiveSmallIntegerField(
+        default=2, validators=[MaxValueValidator(3)])
+
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class Category(models.Model):
@@ -36,9 +52,13 @@ class Expense(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     amount = models.IntegerField(blank=False)
+    currency = models.ForeignKey(
+        Currency, on_delete=models.SET_NULL, null=True)
     paid = models.BooleanField(default=False)
     paid_by = models.ForeignKey(
         GroupMember, on_delete=models.SET_NULL, default=None, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
