a1b56dc2147362e7631bf4e6ba7b32795f12c150
Conall <conallimlah@gmail.com>
2023-01-21 22:33:16 +0900

    update user, add signal to create token on create

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -5,4 +5,18 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = "__all__"
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_email(self, value):
+        if not value or value == "":
+            raise serializers.ValidationError("must provide an email address")
+        return value
+
+    def create(self, validated_data):
+        print(validated_data)
+        user = User(email=validated_data["email"],
+                    username=validated_data["username"])
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from utils.queryset import default_user_queryset
+
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return User.objects.all()
+        return User.objects.filter(id=user.id)
