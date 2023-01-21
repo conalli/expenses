4b2e647bcc7d8296986d7860fa3d7d0e9bac2fe4
Conall <conallimlah@gmail.com>
2023-01-21 22:31:57 +0900

    update views

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,13 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.request import Request
-from rest_framework.response import Response
+from utils.queryset import default_user_queryset
 
 from .models import Category, Currency, Expense
 from .permissions import IsAdminOrReadOnly
 from .serializers import (CategorySerializer, CurrencySerializer,
                           ExpenseSerializer)
-from .utils import QueryParamParser
 
 
 class CurrencyViewSet(viewsets.ModelViewSet):
@@ -24,11 +21,3 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-
-    @action(detail=True)
-    def group(self, request: Request) -> Response:
-        query = request.query_params
-        expense_filter = QueryParamParser(query).to_filter()
-        expenses = Expense.objects.filter(**expense_filter)
-        serializer = ExpenseSerializer(expenses, many=True)
-        return Response(serializer.data)
--- backend/groups/views.py
+++ backend/groups/views.py
@@ -1,21 +1,27 @@
 from django.http import Http404
+from expenses.models import Expense
+from expenses.serializers import ExpenseSerializer
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ParseError, ValidationError
 from rest_framework.parsers import JSONParser
 from rest_framework.request import Request
 from rest_framework.response import Response
+from utils.queryset import default_user_queryset
 
 from .metadata import GroupMetadata
 from .models import Group, GroupMember
 from .serializers import GroupMemberSerializer, GroupSerializer
+from .utils import QueryParamParser
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     """ APIView for managing groups """
-    queryset = Group.objects.all()
     serializer_class = GroupSerializer
     metadata_class = GroupMetadata
+
+    def get_queryset(self):
+        return default_user_queryset(self, Group, "members", "members")
 
     def get_group(self, pk: int) -> Group:
         try:
@@ -50,7 +56,17 @@ class GroupViewSet(viewsets.ModelViewSet):
         serializer = GroupMemberSerializer(members, many=True)
         return Response(serializer.data)
 
+    @action(detail=True)
+    def expenses(self, request: Request, pk: int) -> Response:
+        query = request.query_params
+        expense_filter = QueryParamParser(pk, query).to_filter()
+        expenses = Expense.objects.filter(**expense_filter)
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response(serializer.data)
+
 
 class GroupMemberViewSet(viewsets.ModelViewSet):
-    queryset = GroupMember.objects.all()
     serializer_class = GroupMemberSerializer
+
+    def get_queryset(self):
+        return default_user_queryset(self, GroupMember, "user", None)
